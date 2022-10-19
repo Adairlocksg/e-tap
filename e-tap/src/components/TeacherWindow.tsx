95ed92fa8589b60ee476a3694f49be407101fec8
eduardodgdebem <bemeduardode@gmail.com>
2022-10-19 20:10:41 -0300

    feature/iniciando form direito

--- e-tap/src/components/TeacherWindow.tsx
+++ e-tap/src/components/TeacherWindow.tsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
 import GenericWindow from "./GenericWindow";
 
+type teacher = {
+  name: string;
+  lastname: string;
+  email: string;
+  password: string;
+};
+
 const TeacherWindow = () => {
+  const [teacher, setTeacher] = useState<teacher>({
+    name: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChangeTeacher = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+
+    setTeacher({
+      ...teacher,
+      [name]: e.target.value,
+    });
+    console.log(teacher);
+  };
+
   const FormTeacher = () => {
     return (
       <div className="form-control">
@@ -8,32 +33,44 @@ const TeacherWindow = () => {
           <span className="label-text">Nome</span>
           <input
             type="text"
+            name="name"
+            value={teacher.name}
             placeholder="Escreva aqui"
+            onChange={handleChangeTeacher}
             className="input input-bordered w-full mb-4"
           />
           <span className="label-text">Sobrenome</span>
           <input
             type="text"
+            name="lastname"
+            value={teacher.lastname}
             placeholder="Escreva aqui"
+            onChange={handleChangeTeacher}
             className="input input-bordered w-full mb-4"
           />
 
           <span className="label-text">E-mail</span>
           <input
             type="email"
+            name="email"
+            value={teacher.email}
             placeholder="Escreva aqui"
+            onChange={handleChangeTeacher}
             className="input input-bordered w-full mb-4"
           />
           <span className="label-text">Senha</span>
           <input
             type="password"
+            name="password"
+            value={teacher.password}
             placeholder="Escreva aqui"
+            onChange={handleChangeTeacher}
             className="input input-bordered  w-full mb-4"
           />
         </div>
         <div className="modal-action">
           <label htmlFor="my-modal-teacher" className="btn">
-            Yay!
+            Salvar
           </label>
         </div>
       </div>
